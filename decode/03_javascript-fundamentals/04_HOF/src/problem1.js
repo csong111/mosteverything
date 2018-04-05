53c324022a43a827ad528b18c5d9858a08768da6
csong111 <caroline.song@mail.mcgill.ca>
2018-04-05 17:23:10 -0400

    updates

--- decode/03_javascript-fundamentals/04_HOF/src/problem1.js
+++ decode/03_javascript-fundamentals/04_HOF/src/problem1.js
@@ -33,9 +33,9 @@ function callNoNull(f, arg) {
 
 
 function exceptionalize(f) {
-    return function f(arg){}
+    return function f(arg){};
         if (f(arg) === null) throw new Error("jk");
-        return arg;
+        return f(arg);
 }
     // returns a new function
     // this function takes 1 input, called arg
