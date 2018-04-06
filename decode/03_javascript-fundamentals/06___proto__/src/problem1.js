f204536620c2235ad3c0ebfd1caab9667fa6068d
csong111 <caroline.song@mail.mcgill.ca>
2018-04-06 16:07:39 -0400

    updates

--- decode/03_javascript-fundamentals/06___proto__/src/problem1.js
+++ decode/03_javascript-fundamentals/06___proto__/src/problem1.js
@@ -1,7 +1,7 @@
 // Make parent the __proto__ of child
 
 var parent = {x: 5, y: 6, z: 8};
-var child = {x : 10};
-child.__proto__ = parent;
+var child = {x : 10, __proto__: parent};
+//child.__proto__ = parent;
 
 module.exports = {parent, child}
