ce9ef235908c5e4942da8d528d91533a96d04d29
csong111 <caroline.song@mail.mcgill.ca>
2018-04-05 20:03:46 -0400

    updates

--- decode/03_javascript-fundamentals/05_exceptions/src/problem1.js
+++ decode/03_javascript-fundamentals/05_exceptions/src/problem1.js
@@ -9,19 +9,20 @@ function first(arr) {
 
 function detective(i) {
     function suspect(i) {
-        if(i * 7 % 3 == 0) throw new Error("Bad i!");
+        if(i * 7 % 3 == 0) {
+            throw new Error("Bad i!");
+        } else return "everything ok";
+    }
         try {
             suspect();
         }
         catch(err) {
             return "something fishy";
         }
-        return "everything ok";
     }
     // detective checks to see if the suspect throws an exception on input i.
     // Returns "everything ok" if the suspect doesn't. 
     // Returns "something fishy" if the suspect does.
-}
 
 function assignFlight(name) {
     var flightNumber = ((name.length * 7) % 20) + "0";
