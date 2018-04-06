2e69af853e6b99458efc2624a1427342de818078
csong111 <caroline.song@mail.mcgill.ca>
2018-04-05 20:16:46 -0400

    updates

--- decode/03_javascript-fundamentals/05_exceptions/src/problem1.js
+++ decode/03_javascript-fundamentals/05_exceptions/src/problem1.js
@@ -9,14 +9,22 @@ function first(arr) {
 
 function detective(i) {
     function suspect(i) {
-        if(i * 7 % 3 == 0) throw new Error("Bad i!");
         try {
-            detective()
+            suspect();
         }
         catch(err) {
             return "something fishy"
         }
         return "everything ok";
+
+        //if(i * 7 % 3 == 0) throw new Error("Bad i!");
+        //try {
+        //    detective()
+        //}
+        //catch(err) {
+        //    return "something fishy"
+        //}
+        //return "everything ok";
     }
     }
 
