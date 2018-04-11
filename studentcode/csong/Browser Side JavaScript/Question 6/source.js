3f95683bdd75475ab8a9916e4d5111e57eb90517
csong111 <caroline.song@mail.mcgill.ca>
2018-04-11 12:51:27 -0400

    updates

--- studentcode/csong/Browser Side JavaScript/Question 6/source.js
+++ studentcode/csong/Browser Side JavaScript/Question 6/source.js
@@ -1,6 +1,7 @@
 var bod=document.getElementById("bod");
 var clam=document.getElementById("clam");
 var buttonsClicked=false;
+var buttonClicked=false;
 
 var count=0;
 var reset = document.createElement('button');
@@ -8,8 +9,9 @@ var reset = document.createElement('button');
 function reload () {
     window.location.reload();
 }
-
-function youWin() {
+function youWin(clickedButton) {
+    buttonClicked=true;
+    clickedButton.remove();
     count ++;
     if (count == 5) {
     buttonsClicked = true;
@@ -24,7 +26,7 @@ function youWin() {
 function noDice() {
     if (buttonsClicked == false) {
         clam.innerText="better luck next time... or not!"
-        buttons.forEach(() => removeEventListener('click', youWin));
+        buttons.forEach(() => removeEventListener('click', youWin(clickedButton)));
         bod.appendChild(reset);
         reset.innerText="dare to play again?"
         reset.style.backgroundColor='pink';
@@ -33,7 +35,7 @@ function noDice() {
 };
 var buttons = []
 function makeButton () {
-    for (i=0; i<5; i++) {
+    for (let i=0; i<5; i++) {
         buttons[i]=document.createElement('button');
         startButton.remove();
         buttons[i].innerText="~catch me if you can~ ;)";
@@ -42,9 +44,10 @@ function makeButton () {
         buttons[i].style.left=Math.random()*1200+1+"px";
         buttons[i].style.top=Math.random()*600+1+"px";
         buttons[i].style.position='absolute';
-        buttons[i].addEventListener('click', youWin);
-        bod.appendChild(buttons[i]);};
-        setTimeout(noDice, 7000);
+        bod.appendChild(buttons[i]);
+        bod.addEventListener('click', event => noDice)
+        buttons[i].addEventListener('click', () => youWin(buttons[i]));};
+        setTimeout(noDice, 9000);
 }
 
 var startButton=document.createElement('button');
