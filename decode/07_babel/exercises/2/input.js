1c67df4647611d38a96b2cc9023f76f213e67b90
csong111 <caroline.song@mail.mcgill.ca>
2018-04-18 17:07:21 -0400

    updates

---
+++ decode/07_babel/exercises/2/input.js
@@ -0,0 +1,7 @@
+var rootdiv = document.getElementById('root');
+var elem = <ul>
+    <li>cheese</li>
+    <li>bacon</li>
+    <li>milk</li>
+</ul>
+ReactDOM.render(elem, rootdiv);
--- decode/07_babel/exercises/3/output.js
+++ decode/07_babel/exercises/3/output.js
@@ -1,16 +1,16 @@
 var rootdiv = document.getElementById('root');
 var elem = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "a",
-        { href: "www.google.com2" },
-        "Google.com"
+        'a',
+        { href: 'www.google.com' },
+        'Google.com'
     ),
     React.createElement(
-        "div",
-        { id: "somedivid" },
-        "some div"
+        'div',
+        { id: 'somedivid' },
+        'some div'
     )
 );
 ReactDOM.render(elem, rootdiv);
