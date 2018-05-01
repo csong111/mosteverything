b38fd950f15b637fc392ca778482f7732c099cdc
csong111 <caroline.song@mail.mcgill.ca>
2018-05-01 10:37:54 -0400

    updates

--- studentcode/csong/multiple-todo-makeHTTPRequest-master/public/app.js
+++ studentcode/csong/multiple-todo-makeHTTPRequest-master/public/app.js
@@ -10,6 +10,10 @@ function nameInputChanged() {
     setState({ listNameInput: event.target.value });
 }
 
+function importListChanged() {
+    setState({ listRequested: event.target.value });
+//console.log(state.listRequested)
+}
 // Don't try to understand the body of this function. You just 
 // need to understand what each parameter represents
 function makeHTTPRequest(meth, url, body, cb) {
@@ -26,7 +30,8 @@ let state = {
     items: [],
     addItemInput: "", // The contents of the add item input box
     listNameInput: "", // The contents of the input box related to changing the list
-    listName: "grocery list"
+    listName: "grocery list",
+    listRequested: ""
 }
 
 // Calling rerender changes the UI to reflect what's in the state
@@ -36,7 +41,13 @@ function rerender() {
     inputElement.value = state.addItemInput; // you can ignore this line
 
     let listNameElement = document.getElementById('listName')
-    listNameElement.innerText = state.listName;
+    listNameElement.innerText = state.listName
+
+    let listNameInputChanged = document.getElementById('listNameInputChanged')
+    listNameInputChanged.value = state.listNameInput;
+
+    let importListRequested = document.getElementById('importListRequested')
+    importListRequested.value = state.listRequested;
 
     let d = document.getElementById("items");
     d.innerHTML = '';
@@ -49,10 +60,12 @@ function rerender() {
 
 // Our good friend setState paying us a visit from ReactVille
 function setState(newState) {
-    if (newState.items) state.items = newState.items;
-    if (newState.addItemInput) state.addItemInput = newState.addItemInput;
-    if (newState.listNameInput) state.listNameInput = newState.listNameInput;
-    if (newState.listName) state.listName = newState.listName;
+    //if (newState.items) state.items = newState.items;
+    //if (newState.addItemInput) state.addItemInput = newState.addItemInput;
+    //if (newState.listNameInput) state.listNameInput = newState.listNameInput;
+    //if (newState.listName) state.listName = newState.listName;
+    state = {...state, ...newState};
+    //console.log(state)
     rerender();
 }
 
@@ -62,35 +75,120 @@ function sendItemToServer(it, ln) {
         let parsedItems = JSON.parse(itemsFromServer)
         setState({ items: parsedItems })
     }
-    makeHTTPRequest('POST',
-        '/addItem',
-        JSON.stringify({ item: it, listName: ln }),
-        cb)
+    //makeHTTPRequest('POST',
+        //'/addItem',
+        //JSON.stringify({ item: it, listName: ln }),
+        //cb)
+
+    fetch('/addItem', {
+        body: JSON.stringify({ item: it, listName: ln }),
+        method: 'POST'
+    }) 
+        .then (response => response.text())
+        .then(cb)
+}
+
+function sendListToServer(ln) {
+    // This function is so short it could be inlined
+    let cb = (lNFromServer) => {
+        let parsedlN = JSON.parse(lNFromServer)
+        //console.log(parsedlN)
+        // setState({ listName: parsedlN })
+    }
+    //makeHTTPRequest('POST',
+        //'/addList',
+        //JSON.stringify({ listName: ln}),
+        //cb)
+
+    fetch('/addList', {
+        body: JSON.stringify({ listName: ln}),
+        method: 'POST'
+    })
+        .then (response => response.text())
+        .then(cb)
 }
 
 // When you submit the form, it sends the item to the server
 function addItemSubmit() {
     event.preventDefault();
     sendItemToServer(state.addItemInput, state.listName)
+    setState({addItemInput: " "})
 }
 
 function listNameSubmit() {
     event.preventDefault();
-    setState({ listName: state.listNameInput })
+    setState({ listName: state.listNameInput, listNameInput: " "})
     populateItems(state.listName);
+    sendListToServer(state.listName)
 }
 
+function importList() {
+   event.preventDefault();
+   let body = JSON.stringify({listRequested: state.listRequested})
+   let cbb = (res) => {
+    let parsedArr = JSON.parse(res);
+    //console.log(parsedArr)
+    setState({items: parsedArr})
+   }
+   //makeHTTPRequest('POST', '/allList', body, cbb)
+   fetch('/allList', {
+       body: body,
+       method: 'POST'
+   })
+    .then(response=>response.text())
+    .then(cbb)
+}
+
+let cb = itemsString => {
+    let itemsParsed = JSON.parse(itemsString)
+    setState({ items: itemsParsed })
+}
 // When the client starts he needs to populate the list of items
 function populateItems(listName) {
-    let cb = itemsString => {
-        let itemsParsed = JSON.parse(itemsString)
-        setState({ items: itemsParsed })
-    }
     let body = JSON.stringify({ listName: state.listName })
-    makeHTTPRequest('POST', '/items', body, cb)
+    //makeHTTPRequest('POST', '/items', body, cb)
+    fetch('/items', {
+        body: body,
+        method: 'POST'
+    })
+        .then(response => response.text())
+        .then(cb)
 }
 
+function clearList() {
+    let body = JSON.stringify({listName: state.listName})
+    //makeHTTPRequest('POST', '/cleared', body, cb)
+    fetch('/cleared', {
+        body: body,
+        method: 'POST'
+    })
+        .then(response=>response.text())
+        .then(cb)
+}
 
+function reverseList() {
+    let body = JSON.stringify({listName: state.listName})
+    //makeHTTPRequest('POST', '/reversed', body, cb)
+    fetch('/reversed', {
+       body: body,
+       method: 'POST' 
+    })
+        .then(response => response.text())
+        .then(cb)
+}
 
+function showList () {
+    let cbb = listInfo => {
+        //console.log(listInfo)
+        let listInfoParsed = JSON.parse(listInfo)
+        setState({ items: listInfoParsed.it ? listInfoParsed.it : [],  listName: listInfoParsed.ln})}
+    //makeHTTPRequest('GET', '/getItemsLastList', undefined, cbb)
+    fetch('/getItemsLastList', {
+        method: 'GET'
+    })
+        .then(response=>response.text())
+        .then(cbb)
+}
 // We define a function and then call it right away. I did this to give the file a nice structure.
-populateItems(state.listName);
+//populateItems(state.listName)
+showList();
--- studentcode/csong/multiple-todo-makeHTTPRequest-master/server.js
+++ studentcode/csong/multiple-todo-makeHTTPRequest-master/server.js
@@ -12,7 +12,8 @@ app.get('/app.js', (req, res) => res.send(fs.readFileSync('./public/app.js').toS
 
 // 
 let serverState = {
-    items: {}
+    items: {},
+    listName: 'grocery list',
 }
 
 app.post('/items', (req, res) => {
@@ -20,6 +21,7 @@ app.post('/items', (req, res) => {
     let listName = parsedBody.listName;
     let items = serverState.items[listName];
     if(!items) items = []
+    console.log(serverState.items)
     res.send(JSON.stringify(items));
 })
 
@@ -34,8 +36,41 @@ app.post('/addItem', (req, res) => {
     // The following could be rewritten in a shorter way using push.
     // Try rewriting it. It will help you understand it better.
     serverState.items[listName] = serverState.items[listName].concat(parsedBody.item)
+    console.log(serverState.items)
     res.send(JSON.stringify(serverState.items[listName]));
 })
 
+app.post('/addList', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    let listName = parsedBody.listName;
+    if (listName) {serverState.listName=listName}
+    res.send(JSON.stringify(serverState.listName))
+})
+
+app.post('/cleared', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    let listName = parsedBody.listName;
+    serverState.items[listName]=[];
+    res.send(JSON.stringify(serverState.items[listName]));
+})
+
+app.post('/reversed', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    let listName = parsedBody.listName;
+    serverState.items[listName]=serverState.items[listName].reverse();
+    res.send(JSON.stringify(serverState.items[listName]));
+})
+
+app.get('/getItemsLastList', (req, res) => {
+    res.send(JSON.stringify({ln: serverState.listName, it: serverState.items[serverState.listName]}))
+})
+
+app.post('/allList', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    let listRequested = parsedBody.listRequested;
+    console.log(serverState.items[listRequested])
+    serverState.items[serverState.listName] = serverState.items[serverState.listName].concat(serverState.items[listRequested])
+    res.send(JSON.stringify(serverState.items[serverState.listName]));
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
